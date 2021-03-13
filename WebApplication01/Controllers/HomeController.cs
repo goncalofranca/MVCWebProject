cc8d5903923b226f273de4e57b5f0ee015e726c3
Gonçalo França <goncalo.1994@hotmail.com>
2021-03-13 10:24:13 +0000

    Add project files.

---
+++ WebApplication01/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+using WebApplication01.Models;
+
+namespace WebApplication01.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+
+        public string Saudacao()
+        {
+            return "Olá bom dia!";
+        }
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult Contacts()
+        {
+            return Content("Olá aqui são apresentados os contactos");
+            //content retorn um ContentResult que deriva de IActionResult
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
