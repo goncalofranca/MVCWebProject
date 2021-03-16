93d2d5668e1236a6f276350a71a6368d6fc0ba1d
Gonçalo França <goncalo.1994@hotmail.com>
2021-03-16 20:22:57 +0000

    Aula 3 part 1

---
+++ WebApplication01/Controllers/DadosController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication01.Controllers
+{
+    public class DadosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        /// <summary>
+        /// Atenção ao nome dos parametros porque o que for colocado será o que estará na query string
+        /// em vez de nome etiver XYZ na query string no browser estará "..../dados/registaCliente/id=1&XYZ=Zé"
+        /// </summary>
+        /// <param name="Id"></param>
+        /// <param name="Nome"></param>
+        /// <param name="Morada"></param>
+        /// <param name="Idade"></param>
+        /// <returns></returns>
+        public IActionResult RegistaCliente(int Id, string Nome, string Morada, int Idade)
+        {
+            /// para testar
+            /// http://....../Dados/ResgistaCliente?Id=1&Nome=Ze&Morada=Porto&Idade=20
+            /// http://....../Dados/ResgistaCliente?1&Nome=Ze&Morada=Porto&Idade=20
+
+            return View();
+        }
+        public IActionResult MostraRegistoCliente(int Id, string Nome, string Morada, int Idade)
+        {
+            /// para testar
+            /// http://....../Dados/ResgistaCliente?Id=1&Nome=Ze&Morada=Porto&Idade=20
+            /// http://....../Dados/ResgistaCliente?1&Nome=Ze&Morada=Porto&Idade=20
+
+            ViewData.Add("id", Id);
+            ViewData.Add("nome", Nome);
+            ViewData.Add("morada", Morada);
+            ViewData.Add("idade", Idade);
+
+            return View();
+        }
+    }
+}
---
+++ WebApplication01/Controllers/ProdutosController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication01.Controllers
+{
+    public class ProdutosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Saudacao()
+        {
+            return Content("Olá, estás dentro do Produtos Controller e no Mátodo Saudação");
+        }
+
+        public IActionResult ProdutosEmJson()
+        {
+            string[] listProdutos = new string[] { "Limao", "Melao", "Abacaxi" };
+            return Json(listProdutos);
+        }
+
+        /// <summary>
+        /// Faz redirect to another action, in this case Index
+        /// </summary>
+        /// <returns>returns the view wich it will be redirect</returns>
+        public IActionResult ToAntother()
+        {
+            return RedirectToAction("Index");
+        }
+    }
+}
